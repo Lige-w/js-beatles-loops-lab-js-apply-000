72e4a1e51f609531998fa279aaeb3d84ed6a7d28
Lige <lige.williamson@gmail.com>
2019-01-25 19:52:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+const theBeatlesPlay = (musicians, instruments) => {
+  const arr = [];
+  for (let i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return arr;
+}
+
+const johnLennonFacts = (facts) => {
+  let i = 0;
+  let arr = [];
+  while (facts[i]) {
+    arr.push(facts[i] + "!!!");
+    i++
+  }
+  return arr;
+}
